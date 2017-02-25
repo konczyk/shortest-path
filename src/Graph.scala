e713537e3a11dd2648d5883745d7ddf31f9a81c7
Daniel Kończyk <daniel@konczyk.net>
2017-02-25 10:05:50 +0100

    Add Graph data structure

--- src/Graph.scala
+++ src/Graph.scala
@@ -2,3 +2,33 @@ case class Edge[A](weight: Int, head: A) extends Ordered[Edge[A]] {
   override def compare(that: Edge[A]):Int = this.weight - that.weight
 }
 
+class Graph[A](map: Map[A, Set[Edge[A]]]) {
+  def edges(vertex: A): Option[Set[Edge[A]]] = map.get(vertex)
+}
+
+object Graph {
+
+  // expected format
+  // vertex1  weight1,edge1  weight2,edge2 [...]
+  // vertex2  weight1,edge1  weight2,edge2 [...]
+  // [...]
+  // f function converts from String to a desired Vertex format (Int, String...)
+  def build[A](it: Iterator[String], f: String => A): Graph[A] = {
+    build(it, Map.empty[A, Set[Edge[A]]], f)
+  }
+
+  private def build[A](it: Iterator[String], map: Map[A, Set[Edge[A]]],
+      f: String => A): Graph[A] = {
+    if (!it.hasNext) new Graph(map)
+    else {
+      val e = it.next.trim.split("\\s+")
+      val k = f(e(0))
+      val v = e.drop(1).map(_.split(",")).map{
+        case Array(a,b) => Edge(a.toInt, f(b))
+      }.toSet
+      build(it, map + (k -> v), f)
+    }
+  }
+
+}
+
---
+++ test/GraphTest.scala
@@ -0,0 +1,26 @@
+import org.junit.runner.RunWith
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class GraphTest extends FunSuite {
+
+  test("compare edges") {
+    val e1 = Edge(5,1)
+    val e2 = Edge(2,3)
+    val e3 = Edge(2,2)
+
+    assert((e1 > e2) === true)
+    assert((e3 < e1) === true)
+  }
+
+  test("build graph") {
+    val g = "1 2,3 4,5; 3 3,4"
+    val graph = Graph.build(g.split(";").toIterator, _.toInt)
+
+    assert(graph.edges(1) === Some(Set(Edge(2,3), Edge(4,5))))
+    assert(graph.edges(3) === Some(Set(Edge(3,4))))
+    assert(graph.edges(2) === None)
+  }
+
+}
